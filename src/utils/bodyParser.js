a1a7eebc4754973eb7c86d95c6ab18f6c8491c3c
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-08-04 12:56:43 -0400

    feat: agregar límite de tamaño para el cuerpo de las peticiones HTTP

--- src/utils/bodyParser.js
+++ src/utils/bodyParser.js
@@ -8,11 +8,22 @@
  * @param {Object} req - Objeto de petición HTTP
  * @returns {Promise<Object>} - Promesa que resuelve con el body parseado
  */
-const parseBody = (req) => {
+const parseBody = (req, options = {}) => {
+  const limitBytes = options.limitBytes || 1024 * 1024
+
   return new Promise((resolve, reject) => {
     let body = ''
+    let size = 0
 
     req.on('data', (chunk) => {
+      size += chunk.length
+
+      if (size > limitBytes) {
+        reject(new Error('Payload too large'))
+        req.destroy()
+        return
+      }
+
       body += chunk.toString()
     })
 
